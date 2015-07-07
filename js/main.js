35ffbf45a3c657af682ae92567abf85081196078
Patrick Best <patrickbest@patrickbest.com>
2015-07-07 16:23:49 -0500

    add basic content & style

--- js/main.js
+++ js/main.js
@@ -8,24 +8,4 @@ function l(data) {
 
 $(document).ready(function () {
   'use strict';
-
-});
-
-$(window).load(function () {
-
-  /* PACKERY */
-  var pckry = new Packery( document.getElementById("gallery-container"), {
-    // options
-    itemSelector: '.photo',
-    percentPosition: true,
-    columnWidth: '.photo',
-    gutter: 0
-  });
-
-  // Display color overlay
-  $('.photo').addClass('color-overlay');
-
-  $('#gallery').animate({
-    opacity: 1
-  }, 600);
 });
